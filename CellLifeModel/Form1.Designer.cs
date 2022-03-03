5d4457325755e389821344dca5345bb3b4842a90
Nixion <den-kosarev@mail.ru>
2022-03-03 17:17:25 +0700

    4

---
+++ CellLifeModel/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace CellLifeModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textLog = new System.Windows.Forms.RichTextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.numRows = new System.Windows.Forms.NumericUpDown();
+            this.numColumns = new System.Windows.Forms.NumericUpDown();
+            this.numTimer = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStep = new System.Windows.Forms.Button();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.timerDraw = new System.Windows.Forms.Timer(this.components);
+            this.picAutomat = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timerCodeWaiting = new System.Windows.Forms.Timer(this.components);
+            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
+            this.labelGen = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numColumns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimer)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picAutomat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textLog);
+            this.groupBox1.Controls.Add(this.btnGen);
+            this.groupBox1.Controls.Add(this.numRows);
+            this.groupBox1.Controls.Add(this.numColumns);
+            this.groupBox1.Controls.Add(this.numTimer);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 245);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки";
+            // 
+            // textLog
+            // 
+            this.textLog.DetectUrls = false;
+            this.textLog.Location = new System.Drawing.Point(6, 45);
+            this.textLog.Name = "textLog";
+            this.textLog.ReadOnly = true;
+            this.textLog.Size = new System.Drawing.Size(200, 89);
+            this.textLog.TabIndex = 3;
+            this.textLog.Text = "Click generate for table generation";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(6, 193);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(200, 44);
+            this.btnGen.TabIndex = 8;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // numRows
+            // 
+            this.numRows.Location = new System.Drawing.Point(86, 167);
+            this.numRows.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numRows.Name = "numRows";
+            this.numRows.Size = new System.Drawing.Size(120, 20);
+            this.numRows.TabIndex = 7;
+            this.numRows.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numColumns
+            // 
+            this.numColumns.Location = new System.Drawing.Point(86, 140);
+            this.numColumns.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numColumns.Name = "numColumns";
+            this.numColumns.Size = new System.Drawing.Size(120, 20);
+            this.numColumns.TabIndex = 6;
+            this.numColumns.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numTimer
+            // 
+            this.numTimer.Location = new System.Drawing.Point(86, 19);
+            this.numTimer.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numTimer.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numTimer.Name = "numTimer";
+            this.numTimer.Size = new System.Drawing.Size(120, 20);
+            this.numTimer.TabIndex = 5;
+            this.numTimer.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Rows";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Columns";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Timer Tick";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStep);
+            this.groupBox2.Controls.Add(this.btnStartStop);
+            this.groupBox2.Location = new System.Drawing.Point(12, 263);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(212, 78);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Управление";
+            // 
+            // btnStep
+            // 
+            this.btnStep.Enabled = false;
+            this.btnStep.Location = new System.Drawing.Point(6, 48);
+            this.btnStep.Name = "btnStep";
+            this.btnStep.Size = new System.Drawing.Size(200, 23);
+            this.btnStep.TabIndex = 1;
+            this.btnStep.Text = "Make Step";
+            this.btnStep.UseVisualStyleBackColor = true;
+            this.btnStep.Click += new System.EventHandler(this.btnStep_Click);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Enabled = false;
+            this.btnStartStop.Location = new System.Drawing.Point(6, 19);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(200, 23);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // timerDraw
+            // 
+            this.timerDraw.Tick += new System.EventHandler(this.timerDraw_Tick);
+            // 
+            // picAutomat
+            // 
+            this.picAutomat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picAutomat.BackColor = System.Drawing.Color.White;
+            this.picAutomat.Location = new System.Drawing.Point(230, 12);
+            this.picAutomat.Name = "picAutomat";
+            this.picAutomat.Size = new System.Drawing.Size(542, 537);
+            this.picAutomat.TabIndex = 2;
+            this.picAutomat.TabStop = false;
+            this.picAutomat.Paint += new System.Windows.Forms.PaintEventHandler(this.picAutomat_Paint);
+            this.picAutomat.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picAutomat_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 371);
+            this.label5.MaximumSize = new System.Drawing.Size(212, 106);
+            this.label5.MinimumSize = new System.Drawing.Size(212, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(212, 106);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "You can change cells at generation 0 by clicking on the picture after generation." +
+    " Also you can resize the window to make it bigger.";
+            // 
+            // timerCodeWaiting
+            // 
+            this.timerCodeWaiting.Tick += new System.EventHandler(this.timerCodeWaiting_Tick);
+            // 
+            // timerRefresh
+            // 
+            this.timerRefresh.Interval = 10;
+            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
+            // 
+            // labelGen
+            // 
+            this.labelGen.AutoSize = true;
+            this.labelGen.Location = new System.Drawing.Point(12, 470);
+            this.labelGen.Name = "labelGen";
+            this.labelGen.Size = new System.Drawing.Size(89, 13);
+            this.labelGen.TabIndex = 4;
+            this.labelGen.Text = "Generation: none";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.labelGen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.picAutomat);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Cellular Automaton";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numColumns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimer)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picAutomat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.NumericUpDown numRows;
+        private System.Windows.Forms.NumericUpDown numColumns;
+        private System.Windows.Forms.NumericUpDown numTimer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStep;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Timer timerDraw;
+        private System.Windows.Forms.PictureBox picAutomat;
+        private System.Windows.Forms.RichTextBox textLog;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timerCodeWaiting;
+        private System.Windows.Forms.Timer timerRefresh;
+        private System.Windows.Forms.Label labelGen;
+    }
+}
+
---
+++ CellLifeModel/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CellLifeModel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int picturePadding = 1;
+        int minX = 0, minY = 0, maxX = 0, maxY = 0;
+        int Columns = 5, Rows = 5;
+
+        bool readyFirst = false;
+        bool resetDraw = false;
+
+        bool CanHandDraw = false;
+        bool CanClick = false;
+        bool CanContinueStep = false;
+        bool inDrawLifePhase = false;
+
+        bool DrawStep = false;
+
+        bool NeedNewMap = false;
+
+        int ClickX = 0, ClickY = 0;
+
+        float[] ColumnsWidth;
+        float RowsHeight = 0;
+
+        bool[,] Cells;
+
+        int currentIter = 0;
+
+        int RoWForDraw = 0;
+
+        private void picAutomat_Paint(object sender, PaintEventArgs e)
+        {
+            if (!readyFirst) return;
+
+            if (resetDraw)
+            {
+                if (picAutomat.Image == null) picAutomat.Image = new Bitmap(picAutomat.Width, picAutomat.Height);
+
+                if (NeedNewMap)
+                {
+                    picAutomat.Image.Dispose();
+                    picAutomat.Image = null;
+                    picAutomat.Image = new Bitmap(picAutomat.Width, picAutomat.Height);
+                    NeedNewMap = false;
+                }
+
+
+                ColumnsWidth = new float[(int)numColumns.Value];
+
+                int ColumnLines = Columns + 1;
+                int RowLines = Rows + 1;
+
+                int drawWidth = maxX - minX;
+                int drawHeight = maxY - minY;
+
+                float dx = (float)drawWidth / (float)(ColumnLines - 1);
+                float dy = (float)drawHeight / (float)(RowLines - 1);
+
+                RowsHeight = (float)maxY / (float)(RowLines - 1);
+
+                float oldX = 0;
+
+                Graphics g = Graphics.FromImage(picAutomat.Image);
+                g.Clear(Color.White);
+
+                Pen p = new Pen(Color.Black, 1);
+
+                for (int i = 0; i < ColumnLines; i++)
+                {
+                    float drawX = dx * i + picturePadding;
+
+                    if (i != 0) ColumnsWidth[i - 1] = oldX; //lift side of column
+
+                    oldX = drawX;
+
+                    g.DrawLine(p, drawX, minY, drawX, maxY);
+                }
+
+                for (int i = 0; i < RowLines; i++)
+                {
+                    float drawY = dy * i + picturePadding;
+
+                    g.DrawLine(p, minX, drawY, maxX, drawY);
+                }
+
+                return;
+            }
+            else if (CanHandDraw)
+            {
+
+                int ColumnLines = Columns + 1;
+                int RowLines = Rows + 1;
+
+                int drawWidth = maxX - minX;
+                int drawHeight = maxY - minY;
+
+                float dx = (float)drawWidth / (float)(ColumnLines -1 );
+                float dy = (float)drawHeight / (float)(RowLines - 1);
+
+                Graphics g = Graphics.FromImage(picAutomat.Image);
+                Pen p = new Pen(Color.Black, 1);
+
+                for (int i = 0; i < Columns; i++)
+                {
+
+                    float yToDraw = dy * RoWForDraw + picturePadding;
+                    //float drawY = ;
+
+                    SolidBrush greenBrush = new SolidBrush(Color.Green);
+                    SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+                    if (!Cells[RoWForDraw,i])
+                    {
+                        g.FillRectangle(whiteBrush, ColumnsWidth[i], yToDraw, dx, dy);
+                    }
+                    else
+                    {
+                        g.FillRectangle(greenBrush, ColumnsWidth[i], yToDraw, dx, dy);
+                    }
+
+                    g.DrawRectangle(p, ColumnsWidth[i], yToDraw, dx, dy);
+                }
+
+                //g.Save(); 
+                return;
+            }
+            else if(DrawStep)
+            {
+
+                int ColumnLines = Columns + 1; //Add number column
+                int RowLines = Rows + 1;
+
+                int drawWidth = maxX - minX;
+                int drawHeight = maxY - minY;
+
+                float dx = (float)drawWidth / (float)(ColumnLines - 1);
+                float dy = (float)drawHeight / (float)(RowLines - 1);
+
+                Graphics g = Graphics.FromImage(picAutomat.Image);
+                Pen p = new Pen(Color.Black, 1);
+
+                for (int i = 0; i < Rows; i++)
+                    for (int j = 0; j < Columns; j++)
+                    {
+                        float yToDraw = dy * i + picturePadding;
+
+                        SolidBrush greenBrush = new SolidBrush(Color.Green);
+                        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+                        if (!Cells[i, j])
+                        {
+                            g.FillRectangle(whiteBrush, ColumnsWidth[j], yToDraw, dx, dy);
+                        }
+                        else
+                        {
+                            g.FillRectangle(greenBrush, ColumnsWidth[j], yToDraw, dx, dy); //
+                        }
+
+                        g.DrawRectangle(p, ColumnsWidth[j], yToDraw, dx, dy);
+                    }
+                return;
+            }
+
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (!inDrawLifePhase)
+            {
+                CanClick = false;
+                btnStartStop.Text = "Stop";
+                inDrawLifePhase = true;
+                CanContinueStep = false;
+
+                timerDraw.Interval = (int)numTimer.Value;
+                timerDraw.Start();
+
+                btnStep.Enabled = false;
+                btnGen.Enabled = false;
+
+                textLog.Text = "Modeling in time...";
+            }
+            else
+            {
+                btnStartStop.Text = "Start";
+                inDrawLifePhase = false;
+                CanContinueStep = true;
+
+                timerDraw.Stop();
+                btnStep.Enabled = true;
+                btnGen.Enabled = true;
+
+                textLog.Text = "Stoped...";
+            }
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            currentIter = 0;
+
+            minX = 0 + picturePadding;
+            minY = 0 + picturePadding;
+
+            maxX = picAutomat.Width - picturePadding;
+            maxY = picAutomat.Height - picturePadding;
+
+            Columns = (int)numColumns.Value;
+            Rows = (int)numRows.Value;
+
+            Cells = new bool[Rows, Columns];
+            for (int i = 0; i < Rows; i++)
+                for(int j = 0;j<Columns;j++) Cells[i,j] = false;
+
+            readyFirst = true;
+            resetDraw = true;
+            picAutomat.Invalidate();
+
+            timerCodeWaiting.Start();
+
+            textLog.Text = "Generation successful!";
+            labelGen.Text = "Generation: none";
+        }
+
+        private void timerCodeWaiting_Tick(object sender, EventArgs e)
+        {
+            picAutomat.Refresh();
+            resetDraw = false;
+            CanClick = true;
+            CanHandDraw = true;
+            CanContinueStep = true;
+
+            timerCodeWaiting.Stop();
+
+            btnStartStop.Enabled = true;
+            btnStep.Enabled = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            btnGen.Enabled = true;
+            btnStartStop.Enabled = false;
+            btnStep.Enabled= false;
+            NeedNewMap = true;
+
+            textLog.Text = "Form was resized, please regenerate your table!";
+            timerDraw.Stop();
+        }
+
+        private void btnStep_Click(object sender, EventArgs e)
+        {
+            if (!CanContinueStep) return;
+
+            CanClick = false;
+            inDrawLifePhase = false;
+            CanContinueStep = true;
+
+            btnStep.Enabled = true;
+            btnGen.Enabled = true;
+
+            textLog.Text = "Modeling Step...";
+            MakeStep();
+        }
+
+        private void picAutomat_MouseClick(object sender, MouseEventArgs e)
+        {
+            bool reDraw = false;
+            if (e != null && CanClick)
+            {
+                ClickX = e.X;
+                ClickY = e.Y;
+
+                int RowToDraw = 0;
+
+                for(int i = 0; i < Rows; i++)
+                {
+                    if((float)ClickY < (float)((RowsHeight*(i+1)) + picturePadding))
+                    {
+                        RowToDraw = i;
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < Columns; i++)
+                {
+                    float miX = ColumnsWidth[i];
+                    float maX = 0;
+
+                    float miY = picturePadding+RowsHeight*RowToDraw;
+                    float maY = 0;
+
+                    if (RowToDraw == (Rows - 1)) maY = RowsHeight * RowToDraw + (RowsHeight * RowToDraw - RowsHeight * (RowToDraw - 1)) + picturePadding;
+                    else maY = picturePadding + RowsHeight * (RowToDraw+1);
+
+                    if (i == Columns - 1) maX = ColumnsWidth[i] + (ColumnsWidth[i] - ColumnsWidth[i-1]);
+                    else maX = ColumnsWidth[i + 1];
+
+                    if ((miX <= ClickX && maX >= ClickX) && (miY <= ClickY && maY >= ClickY))
+                    {
+                        Cells[RowToDraw, i] = !Cells[RowToDraw, i];
+                        reDraw = true;
+                    }
+                }
+
+                RoWForDraw = RowToDraw;
+                if (reDraw) picAutomat.Invalidate();
+                timerRefresh.Start();
+            }
+        }
+        private void timerRefresh_Tick(object sender, EventArgs e)
+        {
+            picAutomat.Refresh();
+            timerRefresh.Stop();
+        }
+
+        private void timerDraw_Tick(object sender, EventArgs e)
+        {
+            MakeStep();
+        }
+
+        private void MakeStep()
+        {
+            CanHandDraw = false;
+            bool[,] CellsNew = new bool[Rows,Columns];
+
+            int[] CellsIds;
+            CellsIds = new int[9];
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Columns; j++)
+                {
+                    if (i == 0) //x = 5(Row) * 10000 + y(Column)
+                    {
+                        CellsIds[0] = (j == 0) ? (Rows - 1) * 10000 + Columns - 1 : (Rows - 1) * 10000 + j - 1;
+                        CellsIds[1] = (Rows - 1) * 10000 + j;
+                        CellsIds[2] = (j == (Columns - 1)) ? (Rows - 1) * 10000 + 0 : (Rows - 1) * 10000 + j + 1;
+                    }
+                    else
+                    {
+                        CellsIds[0] = (j == 0) ? (i - 1) * 10000 + Columns - 1 : (i - 1) * 10000 + j - 1;
+                        CellsIds[1] = (i - 1) * 10000 + j;
+                        CellsIds[2] = (j == (Columns - 1)) ? (i - 1) * 10000 + 0 : (i - 1) * 10000 + j + 1;
+                    }
+
+                    CellsIds[3] = (j == 0) ? i * 10000 + Columns - 1 : i * 10000 + j - 1;
+                    CellsIds[4] = i * 10000 + j;
+                    CellsIds[5] = (j == (Columns - 1)) ? i * 10000 + 0 : i * 10000 + j + 1;
+
+                    if (i == (Rows - 1))
+                    {
+                        CellsIds[6] = (j == 0) ? 0 * 10000 + Columns - 1 : 0 * 10000 + j - 1;
+                        CellsIds[7] = 0 * 10000 + j;
+                        CellsIds[8] = (j == (Columns - 1)) ? 0 * 10000 + 0 : 0 * 10000 + j + 1;
+                    }
+                    else
+                    {
+                        CellsIds[6] = (j == 0) ? (i + 1) * 10000 + Columns - 1 : (i + 1) * 10000 + j - 1;
+                        CellsIds[7] = (i + 1) * 10000 + j;
+                        CellsIds[8] = (j == (Columns - 1)) ? (i + 1) * 10000 + 0 : (i + 1) * 10000 + j + 1;
+                    }
+
+                    int AliveNeighbours = 0;
+                    bool SelfAlive = false;
+
+                    for (int k = 0; k < 9; k++)
+                        if (k == 4) SelfAlive = Cells[CellsIds[k] / 10000, CellsIds[k] % 10000];
+                        else if (Cells[CellsIds[k] / 10000, CellsIds[k] % 10000]) AliveNeighbours++;
+
+                    if (SelfAlive)
+                    {
+                        if (AliveNeighbours < 2) CellsNew[i, j] = false;
+                        else if (AliveNeighbours == 2 || AliveNeighbours == 3) CellsNew[i, j] = true;
+                        else if (AliveNeighbours > 3) CellsNew[i, j] = false;
+                    }
+                    else if (AliveNeighbours == 3) CellsNew[i, j] = true;
+                    else CellsNew[i, j] = false;
+                }
+            
+
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Columns; j++)
+                    Cells[i,j] = CellsNew[i,j];
+
+            DrawStep = true;
+            picAutomat.Invalidate();
+            timerRefresh.Start();
+
+            currentIter++;
+           
+            labelGen.Text = "Generation: " + currentIter;
+        }
+    }
+}

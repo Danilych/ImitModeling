8de9369345ac7c8ce0a8d54b85b3f86326d39fda
Nixion <den-kosarev@mail.ru>
2022-05-13 06:13:47 +0700

    added FootballRandom

---
+++ FootballRandom/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Random8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTrialsLog = new System.Windows.Forms.Label();
+            this.btnTrialsStart = new System.Windows.Forms.Button();
+            this.dgvProbData = new System.Windows.Forms.DataGridView();
+            this.clmName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmGoals = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmPoints = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProbData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTrialsLog
+            // 
+            this.lbTrialsLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTrialsLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbTrialsLog.Location = new System.Drawing.Point(391, 280);
+            this.lbTrialsLog.Name = "lbTrialsLog";
+            this.lbTrialsLog.Size = new System.Drawing.Size(185, 200);
+            this.lbTrialsLog.TabIndex = 4;
+            this.lbTrialsLog.Text = "Ожидание старта";
+            // 
+            // btnTrialsStart
+            // 
+            this.btnTrialsStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTrialsStart.Location = new System.Drawing.Point(391, 12);
+            this.btnTrialsStart.Name = "btnTrialsStart";
+            this.btnTrialsStart.Size = new System.Drawing.Size(185, 30);
+            this.btnTrialsStart.TabIndex = 3;
+            this.btnTrialsStart.Text = "Начать чемпионат";
+            this.btnTrialsStart.UseVisualStyleBackColor = true;
+            this.btnTrialsStart.Click += new System.EventHandler(this.btnTrialsStart_Click);
+            // 
+            // dgvProbData
+            // 
+            this.dgvProbData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dgvProbData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProbData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmName,
+            this.clmGoals,
+            this.clmPoints});
+            this.dgvProbData.Location = new System.Drawing.Point(12, 12);
+            this.dgvProbData.Name = "dgvProbData";
+            this.dgvProbData.Size = new System.Drawing.Size(344, 468);
+            this.dgvProbData.TabIndex = 0;
+            // 
+            // clmName
+            // 
+            this.clmName.HeaderText = "Название команды";
+            this.clmName.Name = "clmName";
+            this.clmName.Width = 150;
+            // 
+            // clmGoals
+            // 
+            this.clmGoals.HeaderText = "Среднее количество голов";
+            this.clmGoals.Name = "clmGoals";
+            // 
+            // clmPoints
+            // 
+            this.clmPoints.HeaderText = "Очки";
+            this.clmPoints.Name = "clmPoints";
+            this.clmPoints.ReadOnly = true;
+            this.clmPoints.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 492);
+            this.Controls.Add(this.lbTrialsLog);
+            this.Controls.Add(this.btnTrialsStart);
+            this.Controls.Add(this.dgvProbData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Футбольный менеджер";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProbData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvProbData;
+        private System.Windows.Forms.Label lbTrialsLog;
+        private System.Windows.Forms.Button btnTrialsStart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmGoals;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmPoints;
+    }
+}
+
---
+++ FootballRandom/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrialsStart_Click(object sender, EventArgs e)
+        {
+            lbTrialsLog.Text = "";
+
+            int rowsCount = dgvProbData.Rows.Count - 1;
+
+            if (rowsCount == 0)
+            {
+                lbTrialsLog.Text = "Не указаны комманды и их среднее количество голов";
+                return;
+            }
+
+            double[] commandsMediumGoals = new double[rowsCount];
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                commandsMediumGoals[i] = Convert.ToDouble(dgvProbData.Rows[i].Cells[1].Value);
+                dgvProbData.Rows[i].Cells[2].Value = 0;
+            }
+
+            //Stages start
+
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < rowsCount-1; j++)
+                {
+                    int leftTeamGoalsAmount = 0;
+
+                    double S = 0f;
+                    double a = 0f;
+
+                    while(S >= -commandsMediumGoals[j])
+                    {
+                        a = rand.NextDouble();
+
+                        S += Math.Log(a);
+
+                        if (S >= -commandsMediumGoals[j]) leftTeamGoalsAmount++;
+                    }
+
+                    for(int k  = j+1; k < rowsCount; k++)
+                    {
+                        int rightTeamGoalsAmount = 0;
+
+                        S = 0f;
+                        a = 0f;
+
+                        while (S >= -commandsMediumGoals[k])
+                        {
+                            a = rand.NextDouble();
+
+                            S += Math.Log(a);
+
+                            if (S >= -commandsMediumGoals[k]) rightTeamGoalsAmount++;
+                        }
+
+                        int leftTeamPoints = Convert.ToInt32(dgvProbData.Rows[j].Cells[2].Value);
+                        int rightTeamPoints = Convert.ToInt32(dgvProbData.Rows[k].Cells[2].Value);
+
+                        if (leftTeamGoalsAmount > rightTeamGoalsAmount) leftTeamPoints += 3;
+                        else if (rightTeamGoalsAmount > leftTeamGoalsAmount) rightTeamPoints += 3;
+                        else if(leftTeamGoalsAmount == rightTeamGoalsAmount)
+                        {
+                            leftTeamPoints++;
+                            rightTeamPoints++;
+                        }
+
+                        dgvProbData.Rows[j].Cells[2].Value = leftTeamPoints;
+                        dgvProbData.Rows[k].Cells[2].Value = rightTeamPoints;
+                    }
+                }
+            }
+
+            dgvProbData.Sort(clmPoints, ListSortDirection.Descending);
+
+            lbTrialsLog.Text = "Выйграла команда " + dgvProbData.Rows[0].Cells[0].Value;
+        }
+    }
+}

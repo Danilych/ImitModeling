879d3e3fe17e0ce5d15ccabbc6cfb08873512072
Nixion <den-kosarev@mail.ru>
2022-05-13 15:04:57 +0700

    Added  lab11

---
+++ Random11/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Random8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnTrialsStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudTrialsAmount = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbChi = new System.Windows.Forms.RichTextBox();
+            this.lbVariance = new System.Windows.Forms.Label();
+            this.lbAverage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudMathExpect = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudMathVariance = new System.Windows.Forms.NumericUpDown();
+            this.chartNormal = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTrialsAmount)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMathExpect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMathVariance)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartNormal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTrialsStart
+            // 
+            this.btnTrialsStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnTrialsStart.Location = new System.Drawing.Point(16, 214);
+            this.btnTrialsStart.Name = "btnTrialsStart";
+            this.btnTrialsStart.Size = new System.Drawing.Size(75, 30);
+            this.btnTrialsStart.TabIndex = 3;
+            this.btnTrialsStart.Text = "Начать";
+            this.btnTrialsStart.UseVisualStyleBackColor = true;
+            this.btnTrialsStart.Click += new System.EventHandler(this.btnTrialsStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество испытаний";
+            // 
+            // nudTrialsAmount
+            // 
+            this.nudTrialsAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudTrialsAmount.Location = new System.Drawing.Point(16, 35);
+            this.nudTrialsAmount.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudTrialsAmount.Name = "nudTrialsAmount";
+            this.nudTrialsAmount.Size = new System.Drawing.Size(120, 26);
+            this.nudTrialsAmount.TabIndex = 1;
+            this.nudTrialsAmount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.lbChi);
+            this.panel1.Controls.Add(this.lbVariance);
+            this.panel1.Controls.Add(this.lbAverage);
+            this.panel1.Location = new System.Drawing.Point(218, 355);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(325, 125);
+            this.panel1.TabIndex = 1;
+            // 
+            // lbChi
+            // 
+            this.lbChi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbChi.BackColor = System.Drawing.Color.Gainsboro;
+            this.lbChi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbChi.Location = new System.Drawing.Point(15, 79);
+            this.lbChi.Multiline = false;
+            this.lbChi.Name = "lbChi";
+            this.lbChi.ReadOnly = true;
+            this.lbChi.Size = new System.Drawing.Size(288, 26);
+            this.lbChi.TabIndex = 2;
+            this.lbChi.Text = "Chi-squared: ";
+            // 
+            // lbVariance
+            // 
+            this.lbVariance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbVariance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbVariance.Location = new System.Drawing.Point(15, 44);
+            this.lbVariance.Name = "lbVariance";
+            this.lbVariance.Size = new System.Drawing.Size(292, 23);
+            this.lbVariance.TabIndex = 1;
+            this.lbVariance.Text = "Variance:";
+            this.lbVariance.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbAverage
+            // 
+            this.lbAverage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbAverage.Location = new System.Drawing.Point(15, 11);
+            this.lbAverage.Name = "lbAverage";
+            this.lbAverage.Size = new System.Drawing.Size(288, 23);
+            this.lbAverage.TabIndex = 0;
+            this.lbAverage.Text = "Average: ";
+            this.lbAverage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Мат. ожидание";
+            // 
+            // nudMathExpect
+            // 
+            this.nudMathExpect.DecimalPlaces = 3;
+            this.nudMathExpect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudMathExpect.Location = new System.Drawing.Point(17, 88);
+            this.nudMathExpect.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudMathExpect.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.nudMathExpect.Name = "nudMathExpect";
+            this.nudMathExpect.Size = new System.Drawing.Size(120, 26);
+            this.nudMathExpect.TabIndex = 4;
+            this.nudMathExpect.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Дисперсия";
+            // 
+            // nudMathVariance
+            // 
+            this.nudMathVariance.DecimalPlaces = 3;
+            this.nudMathVariance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudMathVariance.Location = new System.Drawing.Point(17, 140);
+            this.nudMathVariance.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudMathVariance.Name = "nudMathVariance";
+            this.nudMathVariance.Size = new System.Drawing.Size(120, 26);
+            this.nudMathVariance.TabIndex = 6;
+            this.nudMathVariance.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // chartNormal
+            // 
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.LabelStyle.Format = "##.###";
+            chartArea1.AxisY.Maximum = 1D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chartNormal.ChartAreas.Add(chartArea1);
+            this.chartNormal.Location = new System.Drawing.Point(218, 12);
+            this.chartNormal.Name = "chartNormal";
+            series1.ChartArea = "ChartArea1";
+            series1.CustomProperties = "LabelStyle=Bottom";
+            series1.IsValueShownAsLabel = true;
+            series1.IsVisibleInLegend = false;
+            series1.Label = "#VAL{N2}";
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsVisibleInLegend = false;
+            series2.Name = "Series2";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chartNormal.Series.Add(series1);
+            this.chartNormal.Series.Add(series2);
+            this.chartNormal.Size = new System.Drawing.Size(653, 337);
+            this.chartNormal.TabIndex = 8;
+            this.chartNormal.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 492);
+            this.Controls.Add(this.chartNormal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudMathVariance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudMathExpect);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnTrialsStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudTrialsAmount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Случайные события";
+            ((System.ComponentModel.ISupportInitialize)(this.nudTrialsAmount)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMathExpect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMathVariance)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartNormal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTrialsStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudTrialsAmount;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox lbChi;
+        private System.Windows.Forms.Label lbVariance;
+        private System.Windows.Forms.Label lbAverage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudMathExpect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudMathVariance;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartNormal;
+    }
+}
+
---
+++ Random11/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double GetPlotnost(double expect, double variance, double x)
+        {
+            double ePower = -((x - expect)) * (x - expect) / (2 * variance);
+            double e = Math.Pow(Math.E, ePower);
+
+            double div = 1f / (Math.Sqrt(variance) * Math.Sqrt(2 * Math.PI));
+
+            return (div * e);
+        }
+
+        private double UseBoxMuller(double a1, double a2)
+        {
+            double leftSide = Math.Sqrt((-2) * Math.Log(a1));
+            double rightSide = Math.Cos(2 * Math.PI * a2);
+
+            return leftSide * rightSide;
+        }
+
+        private void btnTrialsStart_Click(object sender, EventArgs e)
+        {
+            chartNormal.Series[0].Points.Clear();
+            chartNormal.Series[1].Points.Clear();
+
+            //Function of plotnost'
+
+            int N = (int)nudTrialsAmount.Value;
+
+            double mathExpect = (double)nudMathExpect.Value;
+            double mathVariance = (double)nudMathVariance.Value;
+
+            chartNormal.ChartAreas[0].AxisY.Minimum = 0;
+
+            double dx = (chartNormal.ChartAreas[0].AxisX.Maximum - (chartNormal.ChartAreas[0].AxisX.Minimum)) / 100f;
+            double curX = chartNormal.ChartAreas[0].AxisX.Minimum;
+            double maxY = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                double p = GetPlotnost(mathExpect, mathVariance, (curX + dx * i));
+
+                chartNormal.Series[1].Points.AddXY(curX + dx * i, p);
+            }
+
+            //BOX-MULLER Generator
+
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            double[] randomNumbers = new double[N];
+
+            double maxX = double.MinValue;
+            double minX = double.MaxValue;
+
+            for (int i = 0; i < N; i++)
+            {
+                double result = UseBoxMuller(rand.NextDouble(),rand.NextDouble());
+                result = result * mathVariance + mathExpect;
+
+                randomNumbers[i] = result;
+
+                if(result < minX) minX = result;
+                if(result > maxX) maxX = result;
+            }
+
+            chartNormal.ChartAreas[0].AxisX.Minimum = (int)(minX - 1);
+            chartNormal.ChartAreas[0].AxisX.Maximum = (int)(maxX + 1);
+
+            int k = (int)Math.Log(N, 2f) + 1;
+            double dk = (maxX-minX)/(double)k;
+
+            int[] intervalMatches = new int[k];
+            double[] intervalFrequency = new double[k];
+
+            for (int i = 0; i < k; i++) intervalMatches[i] = 0;
+
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < k; j++)
+                {
+                    if(randomNumbers[i] >= minX+dk*j && randomNumbers[i] <= minX + dk * (j+1))
+                        intervalMatches[j]++;
+                }
+
+            for (int j = 0; j < k; j++)
+                intervalFrequency[j] = (double)intervalMatches[j]/(double)N;
+
+            maxY = 0f;
+
+            for (int i = 0; i < k; i++)
+            {
+                double drawX = (minX + dk * i);
+                int id = chartNormal.Series[0].Points.AddXY((drawX), intervalFrequency[i]);
+                
+                if(maxY < intervalFrequency[i]) maxY = intervalFrequency[i];
+            }
+
+            if (maxY > 0.75f) chartNormal.ChartAreas[0].AxisY.Maximum = 1f;
+            else if (maxY > 0.5f) chartNormal.ChartAreas[0].AxisY.Maximum = 0.75f;
+            else if (maxY > 0.25f) chartNormal.ChartAreas[0].AxisY.Maximum = 0.5f;
+            else chartNormal.ChartAreas[0].AxisY.Maximum = 0.25f;
+
+            //Empiric calculations
+
+            double empiricExpect = 0f;
+
+            for (int i = 0; i < N; i++)
+                empiricExpect += randomNumbers[i];
+
+            empiricExpect /= (double)N;
+
+            double empiricVariance = 0f;
+
+            for (int i = 0; i < N; i++)
+                empiricVariance += (randomNumbers[i] * randomNumbers[i]);
+
+            empiricVariance /= (double)N;
+
+            empiricVariance -= (empiricExpect * empiricExpect);
+
+            empiricVariance = Math.Sqrt(empiricVariance);
+
+            //Errors calculations
+
+            double absErrorExpect = Math.Abs(empiricExpect - mathExpect);
+
+            double absErrorVariance = Math.Abs(empiricVariance - mathVariance);
+
+            double relativeErrorExpect = 0f;
+            if (mathExpect == 0) relativeErrorExpect = absErrorExpect / 1;
+            else relativeErrorExpect = absErrorExpect / Math.Abs(mathExpect);
+
+            double relativeErrorVariance = absErrorVariance / Math.Abs(mathVariance);
+
+            lbAverage.Text = String.Format("Average: {0:N2} (error = {1:N2}%)", empiricExpect, relativeErrorExpect*100);
+            lbVariance.Text = String.Format("Variance: {0:N2} (error = {1:N2}%)", empiricVariance, relativeErrorVariance*100);
+
+            //Chi calculations
+
+            double[] pI = new double[k];
+
+            for(int i = 0; i < k ; i++)
+            {
+                float second = (float)(((minX + dk * (i + 1)) + (minX + dk * (i)))/(double)2f);
+                float plot = (float)GetPlotnost(mathExpect, mathVariance, second);
+
+                if (plot < 0.002f) pI[i] = 0f;
+                else pI[i] = dk * plot;
+            }
+
+            float bigX = 0f;
+
+            for (int i = 0; i < k; i++)
+            {
+                float sumPartOne = (float)(intervalMatches[i] * intervalMatches[i]);
+                float partTwo = ((float)N * (float)pI[i]);
+                float result;
+
+                if (partTwo < 0.002f) result = 0f;
+                else result = sumPartOne / partTwo;
+
+                bigX += result;
+            }
+
+            bigX -= N;
+
+            float[] chiTable =
+            {
+            3.841f,
+            5.991f,
+            7.815f,
+            9.488f,
+            11.070f,
+            12.592f,
+            14.067f,
+            15.507f,
+            16.919f,
+            18.307f,
+            19.675f,
+            21.026f,
+            22.362f,
+            23.685f,
+            24.996f,
+            26.296f,
+            27.587f,
+            28.869f,
+            30.144f,
+            31.410f
+            };
+
+            bool isBigger = (bigX > chiTable[k - 2]);
+            lbChi.Clear();
+
+            lbChi.SelectionColor = Color.Black;
+            lbChi.AppendText("Chi-squared: ");
+            lbChi.AppendText(String.Format("{0:N2} > {1:N2}  is ", bigX, chiTable[k - 2]));
+            if (isBigger)
+            {
+                lbChi.SelectionColor = Color.Red;
+                lbChi.AppendText("true");
+            }
+            else
+            {
+                lbChi.SelectionColor = Color.Green;
+                lbChi.AppendText("false");
+            }
+
+        }
+    }
+}
